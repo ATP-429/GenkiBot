23e601bd966d5d0d17a57b28540cc718d4743340
ATP_429 <atpgaming123@gmail.com>
2023-04-21 14:34:04 +0530

    Add scoreboard, slash commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from discord import app_commands
 import discord
 import pickle
 import os
@@ -5,12 +6,14 @@ import os
 
 class Scoreboard:
 	def __init__(self):
-		self.scores = dict()
+		score_file = open("score.txt", "rb")
+		self.scores = pickle.load(score_file)
+		print("Loaded")
 
 	async def loadData(self):
-		if os.path.exists("score.txt"):
-			score_file = open("score.txt", "rb")
-			self.scores = pickle.load(score_file)
+		score_file = open("score.txt", "rb")
+		self.scores = pickle.load(score_file)
+		print("Loaded")
 
 	async def saveData(self):
 		score_file = open("score.txt", "wb")
@@ -21,17 +24,21 @@ class Scoreboard:
 		await self.saveData()
 
 	async def add(self, user_id):
+		if user_id not in self.scores:
+			self.scores[user_id] = 0
 		self.scores[user_id] += 1
 		await self.saveData()
 	
 	async def sub(self, user_id):
+		if user_id not in self.scores:
+			self.scores[user_id] = 0
 		self.scores[user_id] -= 1
 		await self.saveData()
 
 	async def printLeaderboard(self):
-		s = "LEADERBOARD\n"
-		for user_id, score in sorted(self.scores.items(), key=lambda x: x[1]):
-			s += f"{bot.get_user(user_id).name} : {score}"
+		s = "\nSCOREBOARD\n"
+		for user_id, score in sorted(self.scores.items(), key=lambda x: x[1], reverse=True):
+			s += f"{bot.get_user(user_id).name} : {score} lessons completed!\n"
 		return s
 
 f = open("token.txt")
@@ -42,37 +49,57 @@ scoreboard.loadData()
 
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 bot = discord.Client(intents=discord.Intents.all())
+tree = app_commands.CommandTree(bot)
+
+# async def confirm(message):
+# 	await message.add_reaction('👍')
+
+@tree.command(name="add", description="Call this command after you complete a Genki Chapter")
+async def slash_command(interaction: discord.Interaction):
+	global scoreboard
+	await scoreboard.add(interaction.user.id)
+	await interaction.response.send_message("Congrats! "+(await scoreboard.printLeaderboard()))
+
+@tree.command(name="sub", description="Call this command if you want to erase your doing of a Genki Chapter")
+async def slash_command(interaction: discord.Interaction):
+	global scoreboard
+	await scoreboard.sub(interaction.user.id)
+	await interaction.response.send_message("Chapter record deleted. "+(await scoreboard.printLeaderboard()))
+
+@tree.command(name="register", description="Call this command if you want to register yourself on the scoreboard")
+async def slash_command(interaction: discord.Interaction):
+	global scoreboard
+	await scoreboard.register(interaction.user.id)
+	await interaction.response.send_message("Registered!")
+
+@tree.command(name="show", description="Call this command if you want to display the scoreboard")
+async def slash_command(interaction: discord.Interaction):
+	global scoreboard
+	await interaction.response.send_message(await scoreboard.printLeaderboard())
 
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
 async def on_ready():
+	await tree.sync()
 	print("Ready!")
 
-
-async def confirm(message):
-	await message.add_reaction('👍')
-
-
-# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
-@bot.event
-async def on_message(message):
-	global scoreboard
-	if message.channel.name == "scoreboard":
-		if message.content == "!show":
-			await message.channel.send(await scoreboard.printLeaderboard())
-			await confirm(message)
-		if message.content == "!register":
-			await scoreboard.register(message.author.id)
-			await scoreboard.saveData()
-			await confirm(message)
-		if message.content == "!add":
-			await scoreboard.add(message.author.id)
-			await scoreboard.saveData()
-			await confirm(message)
-		if message.content == "!sub":
-			await scoreboard.sub(message.author.id)
-			await scoreboard.saveData()
-			await confirm(message)
+# #EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+# @bot.event
+# async def on_message(message):
+# 	global scoreboard
+# 	if message.channel.name == "scoreboard":
+# 		if message.content == "!show":
+# 			await message.channel.send(await scoreboard.printLeaderboard())
+# 			await confirm(message)
+# 		if message.content == "!register":
+# 			await scoreboard.register(message.author.id)
+# 			await confirm(message)
+# 		if message.content == "!add":
+# 			await scoreboard.add(message.author.id)
+# 			await confirm(message)
+# 		if message.content == "!sub":
+# 			await scoreboard.sub(message.author.id)
+# 			await confirm(message)
 		
 
 # @bot.event
